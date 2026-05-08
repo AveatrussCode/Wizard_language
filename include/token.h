66789ca6d23af7600c3a76314d8679e993467f77
AvestruzCode <dquispellocallasi@gmail.com>
2026-05-08 17:54:34 +0000

    "Implementacion token"

--- include/token.h
+++ include/token.h
@@ -1,5 +1,6 @@
 #ifndef TOKEN_H
 #define TOKEN_H
+#include <string.h>
 
 typedef enum {
     LEFT_PAREN, RIGHT_PAREN,
@@ -18,5 +19,19 @@ typedef enum {
     PRINT, RETURN, THIS,
     TRUE, VAR, WHILE, OTHER, EOF
 } TokenType;
+
+struct token {
+    TokenType type;
+    string lexema;
+    liral literal;
+    int line;
+
+    token(TokenType _type, string _lexema, liral _literal, int _line){
+        type = _type;
+        lexema = _lexema;
+        literal = _literal;
+        line = _line;
+    }
+};
 
 #endif
