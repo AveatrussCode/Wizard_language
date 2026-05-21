998a11cdaa8a8744254b3b59074d2345462e52c7
Mayurisu <albert36wrm0@gmail.com>
2026-05-21 23:30:25 +0000

    agregado hasta el 15.1.1

--- vm.h
+++ vm.h
@@ -4,10 +4,18 @@
 #include "chunk.h"
 
 typedef struct{
-    Chunk* Chunk;
+    Chunk* chunk;
+    uint8_t* ip;
 } VM;
+
+typedef enum {
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR
+} InterpretResult;
 
 void initVM();
 void freeVM();
+InterpretResult interpret(Chunk* chunk);
 
 #endif
