48d1bc87fc1c442358f7eb05e352c05b24c21108
AvestruzCode <dquispellocallasi@gmail.com>
2026-06-02 23:37:56 +0000

    "Compilador"

--- compiler.c
+++ compiler.c
@@ -14,6 +14,13 @@ typedef struct {
 
 Parser parser;
 
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+  return compilingChunk;
+}
+
+
 static void errorAt(Token* token, const char* message) {
   parser.panicMode = true;
   fprintf(stderr, "[line %d] Error", token->line);
@@ -52,14 +59,36 @@ static void consume(TokenType type, const char* message) {
 
   errorAtCurrent(message);
 }
+static void emitByte(uint8_t byte) {
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+  emitByte(byte1);
+  emitByte(byte2);
+}
+
+static void emitReturn() {
+  emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+  emitReturn();
+}
+
+static void expression() {
+  // What goes here?
+}
 
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
+  compilingChunk = chunk;
   parser.hadError = false;
   parser.panicMode = false;
   advance();
   expression();
   consume(TOKEN_EOF, "Expect end of expression.");
+  endCompiler();
   return !parser.hadError;
 }
 static void errorAtCurrent(const char* message) {
