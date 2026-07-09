03f611d419f7e73692d078c0002332cb02c5cd50
AvestruzCode <dquispellocallasi@gmail.com>
2026-07-09 18:07:51 +0000

    Finalisado el cap 25

--- vm.h
+++ vm.h
@@ -9,7 +9,7 @@
 #define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
 
 typedef struct {
-  ObjFunction* function;
+  ObjClosure* closure;
   uint8_t* ip;
   Valux* slots;
 } CallFrame;
@@ -21,6 +21,7 @@ typedef struct{
   Valux* stackTop;
   Table globals;
   Table strings;
+  ObjUpvalue* openUpvalues;
   Obj* objects;
 
 } VM;
